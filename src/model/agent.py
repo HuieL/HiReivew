6717b127c4c7c7d60bcdb2601517624899ee8a19
HuieL <82495316+HuieL@users.noreply.github.com>
2024-10-12 03:40:24 -0400

    Update agent.py

--- src/model/agent.py
+++ src/model/agent.py
@@ -69,8 +69,3 @@ class APIModel:
             thread.join()
         return res_l
 
-## Example Usage:
-# model  = APIModel(model = "gpt-4o-2024-05-13", api_key = OPENAI_API_KEY, api_url = "https://api.openai.com/v1/chat/completions")
-# prompts = ["can you introduce youself?", "what data is today?"]
-# outputs = model.batch_chat(text_batch=prompts, temperature=1)
-# print(outputs)
